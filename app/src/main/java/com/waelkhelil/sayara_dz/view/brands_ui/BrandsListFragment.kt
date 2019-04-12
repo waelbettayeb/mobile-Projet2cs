e801df3f2ae28b01a04828992bafae044d1e2c35
wael <khelilwael@gmail.com>
2019-04-12 16:57:11 +0100

    add brands list top app bar

--- app/src/main/java/com/waelkhelil/sayara_dz/view/brands_ui/BrandsListFragment.kt
+++ app/src/main/java/com/waelkhelil/sayara_dz/view/brands_ui/BrandsListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.waelkhelil.sayara_dz.R
@@ -27,6 +28,7 @@ class BrandsListFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+//        setSupportActionBar(view?.findViewById(R.id.toolbar) as Toolbar)
         val list:List<Brand> = listOf(
             Brand(0, "Toyota", ""),
             Brand(1, "Audi", ""),
