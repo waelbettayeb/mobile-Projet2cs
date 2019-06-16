8e7ff72668473f696358eaff27e40c99428e4bf5
wael <khelilwael@gmail.com>
2019-06-16 14:15:43 +0100

    add user profile menu

--- app/src/main/java/com/waelkhelil/sayara_dz/view/UserContentFragment.kt
+++ app/src/main/java/com/waelkhelil/sayara_dz/view/UserContentFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.waelkhelil.sayara_dz.R
 
 
@@ -20,5 +23,12 @@ class UserContentFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_user_content, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        val navController = Navigation.findNavController(requireActivity(), R.id.nav_main_host_fragment)
+        NavigationUI.setupWithNavController(toolbar, navController)
+        toolbar.inflateMenu(R.menu.menu_user)
+    }
 }
