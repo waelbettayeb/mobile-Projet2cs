f6ea8887d08420c0b77e50d02bbb2ec7b586d325
marwasoumia <marwa.blaugrana@gmail.com>
2019-07-24 01:06:14 +0100

    added making orders backend without ui integration

--- app/src/main/java/com/waelkhelil/sayara_dz/database/model/ModelSearchResult.kt
+++
@@ -1,8 +0,0 @@
-package com.waelkhelil.sayara_dz.database.model
-
-import androidx.lifecycle.LiveData
-
-data class ModelSearchResult(
-    val data: LiveData<Model>,
-    val networkErrors: LiveData<String>
-)
